87cb19e5d00d1f699f32e60e4ee5e328a52533d9
Pauline Nalwanga <paulinon141@gmail.com>
2021-04-28 17:05:19 +0300

    Added fuctionality using tyoescript

--- ultimate.js
+++ ultimate.js
@@ -1,21 +1,11 @@
-
-let Posts = document.querySelector('.inputPost') as HTMLInputElement;
+var Posts = document.querySelector('.inputPost');
 // let blogPosts = document.querySelector('.blogPosts')
 console.log(Posts);
 console.log(Posts.value);
-
 //  const Blog():any =>{
 //     console.log(inputPost.value);
-    
 //     let blogLi = document.createElement('li');
-    
 //     blogLi.innerHTML = `${inputPost.value}<button class="del-btn"> x </button>`
-    
 //     blogPosts.appendChild(blogLi)
-
 //     inputPost.value = "";
-    
 // }
-
-
-
